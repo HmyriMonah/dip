c4976c9cd24010ca8349340529845c7326ee3873
HmyriMonah <71431180+HmyriMonah@users.noreply.github.com>
2021-06-06 20:52:01 +0300

    big update

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,2 +1,113 @@
-let testButton = document.getElementById('buttest');
-console.log(testButton);
+/* eslint-disable no-undef */
+console.log('hello 1232321');
+
+//open form buttons
+const firstThemeOpenButton = document.getElementById('first-html-theme');
+const secondThemeOpenButton = document.getElementById('second-html-theme');
+const thirdThemeOpenButton = document.getElementById('third-html-theme');
+const fourthThemeOpenButton = document.getElementById('fourth-html-theme');
+const fifthThemeOpenButton = document.getElementById('fifth-html-theme');
+
+const formsList = document.getElementsByClassName('theory-form');
+
+
+
+//forms
+const firstTheme = document.getElementById('first-theme-form');
+const secondThemeForm = document.getElementById('second-theme-form');
+const thirdThemeForm = document.getElementById('third-theme-form');
+const fourthThemeForm = document.getElementById('fourth-theme-form');
+const fifthThemeForm = document.getElementById('fifth-theme-form');
+
+console.log(fourthThemeForm);
+
+const content = document.getElementById('main-content');
+
+const listNext = document.getElementsByClassName('next navigation'); //list next buttons
+
+const listPrevious = document.getElementsByClassName('previous navigation'); //list Previous buttons
+
+listNext[0].onclick = function () {
+  firstTheme.classList.remove('theory-form-flex');
+  secondThemeForm.classList.add('theory-form-flex');
+};
+
+listNext[1].onclick = function () {
+  secondThemeForm.classList.remove('theory-form-flex');
+  thirdThemeForm.classList.add('theory-form-flex');
+};
+
+listNext[2].onclick = function () {
+  thirdThemeForm.classList.remove('theory-form-flex');
+  fourthThemeForm.classList.add('theory-form-flex');
+};
+
+listPrevious[0].onclick = function () {
+  secondThemeForm.classList.remove('theory-form-flex');
+  firstTheme.classList.add('theory-form-flex');
+};
+
+listPrevious[1].onclick = function () {
+  thirdThemeForm.classList.remove('theory-form-flex');
+  secondThemeForm.classList.add('theory-form-flex');
+};
+
+listPrevious[2].onclick = function () {
+  fourthThemeForm.classList.remove('theory-form-flex');
+  thirdThemeForm.classList.add('theory-form-flex');
+};
+
+function closeForms(){
+  Array.from(formsList).forEach(elem => {
+    elem.classList.remove('theory-form-flex');
+});
+}
+
+function showContent() {
+  document.body.classList.remove('darkBackGround');
+  content.classList.remove('content-hidden');
+  closeForms();
+}
+
+//close theory form
+document.onclick = function () {
+  console.log(event.target.id);
+  if (event.target.className == 'close' || event.target.className == 'closeForm navigation') {
+    showContent();
+  }
+};
+
+//open first dorm
+firstThemeOpenButton.onclick = function () {
+  firstTheme.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
+};
+
+//open second dorm
+secondThemeOpenButton.onclick = function () {
+  secondThemeForm.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
+};
+
+//open third dorm
+thirdThemeOpenButton.onclick = function () {
+  thirdThemeForm.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
+};
+
+//open fourth dorm
+fourthThemeOpenButton.onclick = function () {
+  fourthThemeForm.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
+};
+
+//open fourth dorm
+fifthThemeOpenButton.onclick = function () {
+  fifthThemeForm.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
+};
--- src/js/function.js
+++ src/js/function.js
@@ -1,35 +1,52 @@
 /* eslint-disable no-undef */
+console.log('hello 1232321');
 
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
-module.exports.totalCostCalculation = function totalCostCalculation(list) {
-  if (isList(list) && list.length > 0) {
-    list = list.map((item) => item.priceTotal).reduce(reducer);
-    return list;
-  }
-  return false;
-};
+const firstThemeButton = document.getElementById('first-html-theme');
+const secondThemeOpenButton = document.getElementById('second-html-theme');
 
-module.exports.calculationTotalPrice = function calculationTotalPrice(list) {
-  if (isList(list) && list.length > 0) {
-    for (let i = 0; i < list.length; i++) {
-      list[i].priceTotal = list[i].count * list[i].priceForOne;
-    }
-    return true;
-  }
-  return false;
-};
-isList = function isList(list) {
-  if (Array.isArray(list) && list.length > 0) {
-    return true;
-  }
-  return false;
+const firstTheme = document.getElementById('first-theme-form');
+const secondTheme = document.getElementById('second-theme-form');
+
+const content = document.getElementById('main-content');
+
+const listNext = document.getElementsByClassName('next navigation'); //list next buttons
+
+const listPrevious = document.getElementsByClassName('previous navigation'); //list Previous buttons
+
+listNext[0].onclick = function () {
+  firstTheme.classList.remove('theory-form-flex');
+  secondTheme.classList.add('theory-form-flex');
 };
 
-module.exports.setCount = function setCount(elementProduct, count) {
-  elementProduct.count = count;
-  return true;
+listPrevious[0].onclick = function () {
+  secondTheme.classList.remove('theory-form-flex');
+  firstTheme.classList.add('theory-form-flex');
 };
-module.exports.setPrice = function setPrice(elementProduct, price) {
-  elementProduct.priceForOne = price;
-  return true;
+
+
+function showContent() {
+  document.body.classList.remove('darkBackGround');
+  content.classList.remove('content-hidden');
+  firstTheme.classList.remove('theory-form-flex');
+  secondTheme.classList.remove('theory-form-flex');
+}
+
+document.onclick = function () {
+  console.log(event.target.id);
+  //close theory form
+  if (event.target.className == 'close' || event.target.className == 'closeForm navigation') {
+    showContent();
+  }
+};
+
+firstThemeButton.onclick = function () {
+  firstTheme.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
+};
+
+secondThemeOpenButton.onclick = function () {
+  secondTheme.classList.add('theory-form-flex');
+  document.body.className = 'darkBackGround';
+  content.className = 'content-hidden';
 };
